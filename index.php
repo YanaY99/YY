7bf0d1c09221b3e2214f1ae680343f5fa2f1d394
Ignatyeff <boroda42017@gmail.com>
2017-09-15 17:19:27 +0300

    drtju
    dfgj

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ echo'Hello world!';
 require 'vendor/autoload.php';
 $app = new\atk4\ui\App('Registration');
 $app->initLayout('Centered');
-$button = add('Button');
+$button = $app->add('Button');
 $button->set('Add Invoice');
 $button->icon = 'plus';
